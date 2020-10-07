daee18128dc04de21d15109bfb950f2bdb17617b
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-10-06 22:57:55 -0400

    dotenv functional

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require("./server");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
