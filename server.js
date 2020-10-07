a21d51ed703ff1b505c401c9d0dcd5430f4918ee
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-10-06 22:31:22 -0400

    created and implemented logger middleware

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
 const server = express();
+const logger = require("./middleware/logger")
+
+server.use(express.json());
+//custom middleware
+server.use(logger("long"))
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
 
-function logger(req, res, next) {}
 
 module.exports = server;
